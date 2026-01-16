6ae03ba939bcc984cb1975acc884e002f192bc2b
Lucas Ramos <lucas.llramos@gmail.com>
2026-01-16 16:24:22 -0300

    deletando arquivos pelo php

---
+++ UNLINK/exemplo-01.php
@@ -0,0 +1,15 @@
+<?php
+ 
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+ $file = fopen("teste.txt","w+");
+
+ fclose($file);
+
+    unlink("teste.txt");
+
+
+ echo "Arquivo removido com sucesso.";
+
+?>
